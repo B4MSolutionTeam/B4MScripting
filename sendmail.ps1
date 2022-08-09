72aaa5250a38726a153e9ad999e26cdde7591c73
Ta Thai Duy <tad6hc@bosch.com>
2022-08-09 21:06:59 +0530

    new ps1 file

---
+++ sendmail.ps1
@@ -0,0 +1,13 @@
+﻿$SmtpServer  = "rb-smtp-int.bosch.com"
+$parameters = @{
+From = ‘Duy.TaThai@vn.bosch.com’
+To = ‘an.phamnhat@vn.bosch.com’
+Subject = ‘Backup Version Overdate 10 year'
+Attachments = @('C:\vdServerAchieve\report.csv')
+Body = “Hello, after examine all file backup version, we have discover these files are overdate. Please kindly review and delete these files. Thank you”
+BodyAsHTML = $True
+Priority = 'High'
+SmtpServer = $SmtpServer
+}
+
+Send-MailMessage @parameters
