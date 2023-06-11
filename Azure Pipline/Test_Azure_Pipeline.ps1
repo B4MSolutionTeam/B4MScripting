f3a0b57c582918b1ac975bdaa7b2daf0e9d2df75
B4MSolutionTeam <Team.B4MOperation@vn.bosch.com>
2023-06-11 22:52:57 +0700

    new commit local

--- Azure Pipline/Test_Azure_Pipeline.ps1
+++ Azure Pipline/Test_Azure_Pipeline.ps1
@@ -1,19 +1,1 @@
 Write-Host($([System.Net.Dns]::GetHostByName($env:computerName).HostName))
-Write-Host "hello"
-
-Write-Host "hahaha"
-Write-Host "dsadads"
-Write-Host "dsadads"
-
-Write-Host "hahaha"
-Write-Host "dsadads"
-Write-Host "dsadads"
-Write-Host "hahaha"
-Write-Host "dsadads"
-Write-Host "dsadads"
-Write-Host "hahaha"
-Write-Host "dsadads"
-Write-Host "dsadads"
-Write-Host "hahaha"
-Write-Host "dsadads"
-Write-Host "dsadads"
